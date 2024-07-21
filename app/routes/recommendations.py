81bc8924e803f442614d256fbcf161280f80f5a0
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2024-07-21 18:20:45 +0530

    Recommendations

---
+++ app/routes/recommendations.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, status, Request, Response, Depends
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.deps import get_db_session
+
+
+router = APIRouter(prefix="recommendation")
+
+
+@router.get("", status_code=status.HTTP_200_OK, tags=["Recommendations"])
+async def get_recommendations(request: Request, response: Response,
+                              db: AsyncSession = Depends(get_db_session)):
+
+    return {"message": "API in works."}
